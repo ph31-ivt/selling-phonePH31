7b416abeacb42d9f899605f8fc1b75371dcf329a
Nhan10 <hovannhan060197@gmail.com>
2019-06-25 15:31:14 +0700

    middleware_admin

---
+++ app/Http/Middleware/IsAdministrator.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\User;
+use Closure;
+use Auth;
+use Symfony\Component\Finder\Exception\AccessDeniedException;
+
+class IsAdministrator
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(!(Auth::user()->user_type == User::USER_ADMIN)){
+            if ($request->ajax()) {
+                return response('Forbidden', 403);
+            }
+            else
+            {
+                throw new AccessDeniedException('Forbidden');
+            }
+        }
+        return $next($request);
+    }
+}
